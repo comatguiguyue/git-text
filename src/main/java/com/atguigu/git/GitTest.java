046c6339aa7ba7744da9ffd13ba5d38ac8fb0891
yanglx <yanglx@qq.com>
2022-08-30 08:49:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author ylxstart
+ * @create 2022-08-30 8:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
